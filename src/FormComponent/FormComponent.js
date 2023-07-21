317fa9c4ed45344e2adefcf4c767c0f1f6ea6e3e
Naveen23072000 <140042025+Naveen23072000@users.noreply.github.com>
2023-07-21 19:38:23 +0530

    Update FormComponent.js

--- src/FormComponent/FormComponent.js
+++ src/FormComponent/FormComponent.js
@@ -1,16 +1,36 @@
 import React from "react";
 export default function FormComponent(props) {
     const { element, formik } = props
-    return (
-        <div id='subjDiv'>
-            <br></br>
-            <h5>{element.SubCode} - {element.SubjectName}</h5>
-            <h5>Faculty: {element.Professor}</h5>
-            <h6>Total Seats:{element.NoOfSeats}</h6>
-            <h6>Available Seats:{element.AvailableSeats}</h6>
-            <h6>Credits: {element.Credits}</h6>
-            <h6>Term: {element.Term}</h6>
-            <input type='number' onChange={formik.handleChange} name={element.SubCode} id={element.SubCode} placeholder="Enter Bid" required="true" min="0" />
-        </div>
-    )
+    if(element.Credits == 3)
+    {
+        return (
+            <div id='subjDiv'>
+                <br></br>
+                <h5>{element.SubCode} - {element.SubjectName}</h5>
+                <h5>Faculty: {element.Professor}</h5>
+                <h6>Total Seats:{element.NoOfSeats}</h6>
+                <h6>Available Seats:{element.AvailableSeats}</h6>
+                <h6>Credits: {element.Credits}</h6>
+                <h6>Term: {element.Term}</h6>
+                <input type='number' onChange={formik.handleChange} name={element.SubCode} id={element.SubCode} placeholder="Enter Bid" required="true" min="0" />
+            </div>
+        )
+    }
+    else
+    {
+        return (
+            <div id='subjDiv'>
+                <br></br>
+                <h5>{element.SubCode} - {element.SubjectName}</h5>
+                <h5>Faculty: {element.Professor}</h5>
+                <h6>Total Seats:{element.NoOfSeats}</h6>
+                <h6>Available Seats:{element.AvailableSeats}</h6>
+                <h6>Credits: {element.Credits}</h6>
+                <h6>Term: 1/2 </h6>
+                <input type='number' onChange={formik.handleChange} name={element.SubCode} id={element.SubCode} placeholder="Enter Bid" required="true" min="0" />
+            </div>
+        )
+
+    }
+    
 }
